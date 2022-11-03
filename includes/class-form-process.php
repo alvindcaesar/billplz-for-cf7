c4c7f3507c9658348c907d43c26f5a1cd44e7651
Alvind <alvind.caesar@gmail.com>
2022-11-04 00:00:26 +0800

    Handle form data submission to process

---
+++ includes/class-form-process.php
@@ -0,0 +1,55 @@
+<?php
+// TODO: wpdb->prepare()
+class Billplz_CF7_Form_Process
+{
+    public function __construct()
+    {
+        add_action('wpcf7_before_send_mail', array($this, 'process_data'));
+    }
+
+    public function process_data($contact_form)
+    {
+        $title = $contact_form->title;
+
+        if ($title === "Payment") {
+            $submission = WPCF7_Submission::get_instance();
+
+            if ($submission) {
+                $form_id        = $submission->get_contact_form()->id;
+                $form_title     = $submission->get_contact_form()->title;
+                $posted_data    = $submission->get_posted_data();
+                $name           = $posted_data['payment-name'];
+                $email          = $posted_data['payment-email'];
+                $amount         = $posted_data['payment-amount'];
+                $phone          = $posted_data['phone-num'];
+                $transaction_id = 'billplz-bill-id';
+                $mode           = 'Live';
+                $status         = 'Pending';
+
+                $this->record_data($form_id, $form_title, $name, $phone, $email, $amount, $transaction_id, $mode, $status);
+            }
+
+        }
+    }
+
+    public function record_data($form_id, $form_title, $name, $phone, $email, $amount, $transaction_id, $mode, $status)
+    {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "billplz_cf7_payment";
+
+        $wpdb->insert($table_name, array(
+            'form_id'        => $form_id,
+            'form_title'     => $form_title,
+            'name'           => $name,
+            'phone'          => $phone,
+            'amount'         => $amount,
+            'transaction_id' => $transaction_id,
+            'email'          => $email,
+            'mode'           => $mode,
+            'status'         => $status,
+            'created_at'     => current_time('mysql'),
+            ),
+        );
+    }
+}
