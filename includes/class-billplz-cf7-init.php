f83298079d0b9b4dd7c83cce2734f76e5c1d1b65
Alvind <alvind.caesar@gmail.com>
2022-11-03 23:55:34 +0800

    Store the class to be instantiated into a variable

--- includes/class-billplz-cf7-init.php
+++ includes/class-billplz-cf7-init.php
@@ -8,10 +8,12 @@ defined("WPINC") or die();
 
 class Billplz_CF7_Init
 {
-  public function __construct()
-  {
-    return array(
-      new Billplz_CF7_Admin_Menu()
-    );
-  }
+    public function __construct()
+    {
+        return array(
+            $billplz_cf7_admin_menu = new Billplz_CF7_Admin_Menu(),
+            $billplz_cf7_payment_db = new Billplz_CF7_Payment_DB(),
+            $billplz_cf7_form_process = new Billplz_CF7_Form_Process(),
+        );
+    }
 }
