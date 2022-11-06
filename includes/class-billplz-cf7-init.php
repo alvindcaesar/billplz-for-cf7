7c62db5130edca617107076df9fe8b4b6e57a19d
Alvind <alvind.caesar@gmail.com>
2022-11-06 15:29:54 +0800

    General Settings implementations :rocket:

--- includes/class-billplz-cf7-init.php
+++ includes/class-billplz-cf7-init.php
@@ -11,9 +11,10 @@ class Billplz_CF7_Init
     public function __construct()
     {
         return array(
-            $billplz_cf7_admin_menu = new Billplz_CF7_Admin_Menu(),
-            $billplz_cf7_payment_db = new Billplz_CF7_Payment_DB(),
-            $billplz_cf7_form_process = new Billplz_CF7_Form_Process(),
+            $billplz_cf7_admin_menu       = new Billplz_CF7_Admin_Menu(),
+            $billplz_cf7_payment_db       = new Billplz_CF7_Payment_DB(),
+            $billplz_cf7_form_process     = new Billplz_CF7_Form_Process(),
+            $billplz_cf7_general_settings = new Billplz_CF7_General_Settings()
         );
     }
 }
