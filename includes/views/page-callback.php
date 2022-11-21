9c1bf96c29e65ba0df2a1daaff588f747f135024
Alvind <alvind.caesar@gmail.com>
2022-11-22 00:36:43 +0800

    Simplify plugin prefix

--- includes/views/page-callback.php
+++ includes/views/page-callback.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h1><?php _e("Billplz for Contact Form 7", BILLPLZ_CF7_TEXT_DOMAIN); ?></h1>
+    <h1><?php _e("Billplz for Contact Form 7", BCF7_TEXT_DOMAIN); ?></h1>
   <?php $active_tab = isset($_GET["tab"])
       ? $_GET["tab"]
       : "payments"; ?>
@@ -19,22 +19,22 @@
   </h2>
 
   <?php if ($active_tab == "payments") {
-    require_once BILLPLZ_CF7_PLUGIN_PATH .
+    require_once BCF7_PLUGIN_PATH .
     "includes/class-billplz-cf7-payment-list.php";
 
-    $billplz_cf7_table = new Billplz_CF7_Payment_List();
+    $bcf7_table = new Billplz_CF7_Payment_List();
     echo "<br>";
-    $billplz_cf7_table->views();
+    $bcf7_table->views();
     // Search function
     echo "<form method='post' name='billplz_search_payment' action='".$_SERVER['PHP_SELF']. "?page=billplz-cf7&tab=payments'>";
-    $billplz_cf7_table->prepare_items();
-    $billplz_cf7_table->search_box( "Search Customer or Bill ID", "payment-search-id");
-    $billplz_cf7_table->display();
+    $bcf7_table->prepare_items();
+    $bcf7_table->search_box( "Search Customer or Bill ID", "payment-search-id");
+    $bcf7_table->display();
     echo "</form>";
   
   } elseif ($active_tab == "general-settings") {
-      require_once BILLPLZ_CF7_PLUGIN_PATH . "includes/views/general-settings-page.php";
+      require_once BCF7_PLUGIN_PATH . "includes/views/general-settings-page.php";
   } else {
-      echo "<p>API Settings goes here</p>";
+    require_once BCF7_PLUGIN_PATH . "includes/views/api-settings-page.php";
   } ?>
 </div>
