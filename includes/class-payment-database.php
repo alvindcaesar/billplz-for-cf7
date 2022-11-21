5ee0178b66656058440ead40683ecc2a6a79cc9c
Alvind <alvind.caesar@gmail.com>
2022-11-22 00:36:58 +0800

    Simplify plugin prefix

--- includes/class-payment-database.php
+++ includes/class-payment-database.php
@@ -9,13 +9,13 @@ class Billplz_CF7_Payment_DB
     {
         global $wpdb;
 
-        $table_name = $wpdb->prefix . "billplz_cf7_payment";
+        $table_name = $wpdb->prefix . "bcf7_payment";
 
         $charsetCollate = $wpdb->get_charset_collate();
 
         if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
 
-          $query = "CREATE TABLE $table_name(
+            $query = "CREATE TABLE $table_name(
             id bigint (20) unsigned NOT NULL AUTO_INCREMENT,
             form_id bigint (20) NOT NULL DEFAULT 0,
             form_title varchar (60) NOT NULL DEFAULT '',
@@ -29,10 +29,10 @@ class Billplz_CF7_Payment_DB
             created_at datetime DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY  (id)
           ) $charsetCollate";
-  
-          require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-          dbDelta($query);
-          
+
+            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+            dbDelta($query);
+
         }
     }
 }
