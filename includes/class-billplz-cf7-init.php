2368830fc5bb58a4021ad6c01d4c1dc9d41a5689
Alvind <alvind.caesar@gmail.com>
2022-11-22 00:36:33 +0800

    Simplify plugin prefix

--- includes/class-billplz-cf7-init.php
+++ includes/class-billplz-cf7-init.php
@@ -11,10 +11,11 @@ class Billplz_CF7_Init
     public function __construct()
     {
         return array(
-            $billplz_cf7_admin_menu       = new Billplz_CF7_Admin_Menu(),
-            $billplz_cf7_payment_db       = new Billplz_CF7_Payment_DB(),
-            $billplz_cf7_form_process     = new Billplz_CF7_Form_Process(),
-            $billplz_cf7_general_settings = new Billplz_CF7_General_Settings()
+            $bcf7_admin_menu = new Billplz_CF7_Admin_Menu(),
+            $bcf7_payment_db = new Billplz_CF7_Payment_DB(),
+            $bcf7_form_process = new Billplz_CF7_Form_Process(),
+            $bcf7_general_settings = new Billplz_CF7_General_Settings(),
+            $bcf7_api_settings = new Billplz_CF7_API_Settings(),
         );
     }
 }
