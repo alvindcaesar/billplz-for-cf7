93f652ef4c4b17243d1a68848e0137df625cacbe
Alvind <alvind.caesar@gmail.com>
2022-11-22 00:40:47 +0800

    General setting page partial

--- includes/views/general-settings-page.php
+++ includes/views/general-settings-page.php
@@ -1,9 +1,7 @@
 <form action="options.php" method="POST">
   <?php
-    settings_fields( "billplz_cf7_group" );
-    do_settings_sections( "billplz_cf7_general_settings" );
+    settings_fields( "bcf7_general" );
+    do_settings_sections( "bcf7_general_settings" );
     submit_button();
   ?>
 </form>
-
-
