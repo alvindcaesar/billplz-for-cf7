c5a0cc55d5c4659aedd48c809143f4dff358481b
Alvind <alvind.caesar@gmail.com>
2022-11-30 01:40:32 +0800

    Reorganize plugin files

---
+++ includes/admin/class-admin-menu.php
@@ -0,0 +1,35 @@
+<?php
+
+defined("WPINC") or die();
+
+class Billplz_CF7_Admin_Menu
+{
+  public function __construct()
+  {
+    add_action("admin_menu", array($this, "add_submenu"));
+  }
+
+  /**
+   * Adds a submenu page under a Contact Form 7 menu.
+   */
+  public function add_submenu()
+  {
+    add_submenu_page(
+      "wpcf7",
+      __("Billplz for Contact Form 7", "billplz-for-cf7"),
+      __("Billplz", "billplz-for-cf7"),
+      "manage_options",
+      "billplz-cf7",
+      array($this, "display_callback")
+    );
+  }
+
+  /**
+   * Display callback for the submenu page.
+   */
+  public function display_callback()
+  {
+    require_once BCF7_PLUGIN_PATH .
+      "includes/views/page-callback.php";
+  }
+}
