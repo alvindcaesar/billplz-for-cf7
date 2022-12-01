f1e5cffe96ff0e1fd09c1ad952cb98c8ea89138c
Alvind <alvind.caesar@gmail.com>
2022-12-01 20:51:18 +0800

    Include the class files inside Billplz_CF7_Init

--- includes/class-billplz-cf7-init.php
+++ includes/class-billplz-cf7-init.php
@@ -4,13 +4,27 @@ class Billplz_CF7_Init
 {
     public function __construct()
     {
+        $this->include_classes();
+        $this->init();
+    }
+
+    public function include_classes()
+    {
+        require_once BCF7_PLUGIN_PATH . "includes/admin/class-admin-menu.php";
+        require_once BCF7_PLUGIN_PATH . "includes/controller/class-form-process.php";
+        require_once BCF7_PLUGIN_PATH . "includes/controller/class-shortcodes.php";
+        require_once BCF7_PLUGIN_PATH . "includes/settings/class-api-settings.php";
+        require_once BCF7_PLUGIN_PATH . "includes/settings/class-general-settings.php";
+    }
+
+    public function init()
+    {
         return array(
-            $bcf7_admin_menu = new Billplz_CF7_Admin_Menu(),
-            $bcf7_payment_db = new Billplz_CF7_Payment_DB(),
-            $bcf7_form_process = new Billplz_CF7_Form_Process(),
+            $bcf7_admin_menu       = new Billplz_CF7_Admin_Menu(),
+            $bcf7_form_process     = new Billplz_CF7_Form_Process(),
+            $bcf7_shortcodes       = Billplz_CF7_Shortcodes::init(),
+            $bcf7_api_settings     = new Billplz_CF7_API_Settings(),
             $bcf7_general_settings = new Billplz_CF7_General_Settings(),
-            $bcf7_api_settings = new Billplz_CF7_API_Settings(),
-            $bcf7_shortcodes = Billplz_CF7_Shortcodes::init()
         );
     }
 }
