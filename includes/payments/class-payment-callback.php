df069d141927f433f9c4f1d9895cb0d8ae0bbc4c
Alvind <alvind.caesar@gmail.com>
2022-12-06 00:13:07 +0800

    Callback handler class for get and post request

--- includes/payments/class-payment-callback.php
+++ includes/payments/class-payment-callback.php
@@ -12,6 +12,19 @@ class Billplz_CF7_Callback
   public static function get()
   {
     if (empty($_GET) or (isset($_GET['post']) and isset($_GET['action']))) return;
+
+    if (isset($_GET['bcf7-listener']) and ("billplz" == $_GET['bcf7-listener'])) {
+      
+      global $wpdb;
+      
+      $table_name = $wpdb->prefix . "bcf7_payment";
+  
+      $status_query = $wpdb->get_results($wpdb->prepare("SELECT status FROM {$table_name} WHERE id= %d", array($_GET['payment-id'])));
+  
+      $status = get_object_vars($status_query[0])['status'];
+  
+      if ("completed" == $status) return;
+    }
         
     $x_signature = bcf7_get_xsignature();
     $url         = htmlentities($_SERVER['QUERY_STRING']);
@@ -38,6 +51,8 @@ class Billplz_CF7_Callback
     unset($query['billplz']['x_signature']);
     unset($query['payment-id']);
     unset($query['bcf7-listener']);
+    unset($query['page_id']);
+
 
     $a = array();
     foreach ($query as $key => $value) {
@@ -52,16 +67,7 @@ class Billplz_CF7_Callback
 
     $hash    = hash_hmac('sha256', $new, $x_signature);
 
-    global $wpdb;
-    
-    $table_name = $wpdb->prefix . "bcf7_payment";
-
-    $status_query = $wpdb->get_results($wpdb->prepare("SELECT status FROM {$table_name} WHERE id= %d", array($payment_id)));
-    $status = get_object_vars($status_query[0])['status'];
-
-    if ("completed" == $status) return;
-
-    if (isset($payment_id) and ($hash == $x_sign) and ('true' == $query['billplz']['paid'])) {
+    if (("billplz" == $_GET['bcf7-listener']) and ($hash == $x_sign) and ('true' == $query['billplz']['paid'])) {
       
       $wpdb->update( $table_name, array( 
           'status' => 'completed', 
@@ -84,8 +90,64 @@ class Billplz_CF7_Callback
     }
   }
 
+/**
+ * Process a callback from Billplz server
+ *
+ * @return void
+ */
   public static function post()
   {
-    // TODO: Finish this method!
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      $query_string = file_get_contents('php://input');
+    
+      parse_str($query_string, $query_params);
+      
+      $x_sign         = $query_params['x_signature'];
+      $x_sign2        = bcf7_get_xsignature();
+      $transaction_id = $query_params['id'];
+      $paid_at        = $query_params['paid_at'];
+      $bill_url       = bcf7_get_url()."/bills/".$transaction_id;
+      $payment_id     = $_GET['payment-id'];
+
+        
+      ksort($query_params);
+      unset($query_params['x_signature']);
+      
+      $a = array();
+      foreach ($query_params as $key => $value) {
+        array_push($a, ($key . $value));
+      }
+
+      sort($a);
+
+      $new = implode('|', $a);
+      
+      $hash = hash_hmac('sha256', $new, $x_sign2);
+
+      global $wpdb;
+      $table_name = $wpdb->prefix . "bcf7_payment";
+      
+      if (("billplz" == $_GET['bcf7-listener']) and ($hash == $x_sign) and ("true" == $query_params['paid'])) {
+        $wpdb->update( $table_name, array( 
+          'status' => 'completed', 
+          'transaction_id' => $transaction_id, 
+          'paid_at' => $paid_at,
+          'bill_url' => $bill_url
+          ), 
+          array( 'ID' => $payment_id ) 
+        );
+
+      } else {
+      
+        $wpdb->update( $table_name, array( 
+            'transaction_id' => $transaction_id, 
+            'paid_at' => '0000-00-00 00:00:00',
+            'bill_url' => $bill_url
+            ), 
+            array( 'ID' => $payment_id )
+        );
+      }
+    }
   }
 }
