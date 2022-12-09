59d242d29f573c4e85f7385f082c2ac759a669f4
Alvind <alvind.caesar@gmail.com>
2022-12-10 02:29:29 +0800

    Flash notice when CF7 not activated and miss creds

--- includes/admin/class-admin-notices.php
+++ includes/admin/class-admin-notices.php
@@ -4,27 +4,35 @@ class Billplz_CF7_Admin_Notices
 {
 	public static function init()
 	{
-		add_action( "admin_notices", array( __CLASS__, "bwpsp_test_mode_warning") );
+		add_action( "admin_notices", array( __CLASS__, "cf7_inactive") );
+		add_action( "admin_notices", array( __CLASS__, "keys_check") );
 	}
 
 	public static function cf7_inactive()
 	{
-		?>
-		<div class="notice notice-error">
-				<p><?php _e( '<strong>Billplz for Contact Form 7 -</strong> Contact Form 7 is not installed and activated. Please install and activate the plugin.', BCF7_TEXT_DOMAIN ); ?></p>
-		</div>
-		<?php
+		if (! function_exists( 'wpcf7')) {
+			echo __(sprintf(
+					'<div class="notice notice-error">
+							<p><strong>Billplz for Contact Form 7 -</strong> Contact Form 7 is not installed and activated. Please install and activate the plugin.</p>
+					</div>',
+			), BCF7_TEXT_DOMAIN );
+		}
 	}
 
-	public static function bwpsp_test_mode_warning()
+	public static function keys_check()
 	{
-		if (! function_exists( 'wpcf7')) {
-		echo __(sprintf(
+		if ( "1" == bcf7_general_option("bcf7_mode") and ((empty(bcf7_api_option("bcf7_sandbox_secret_key"))) or empty(bcf7_api_option("bcf7_sandbox_collection_id")) or empty(bcf7_api_option("bcf7_sandbox_xsignature_key"))) ) {
+			echo __(sprintf(
 				'<div class="notice notice-warning">
-						<p><strong>WPSmartPay: You are using Billplz for WPSmartPay in Test Mode. </strong> Make sure to switch to <a href="' . get_admin_url() . 'admin.php?page=smartpay-setting&tab=gateways">Live Mode</a> when you\'re ready to accept real payments.</p>
-				</div>',
-		), 'billplz-for-smartpay');
+							<p><strong>Billplz for Contact Form 7 -</strong>Billplz Sandbox Credentials is not set. Enter your Secret Key, Collection ID and X-Signature Key in order to use Billplz service. <a href="' . get_admin_url() . 'admin.php?page=billplz-cf7&tab=api-settings">Set Credential</a></p>
+					</div>',
+			), BCF7_TEXT_DOMAIN);
+		} elseif ( "0" == bcf7_general_option("bcf7_mode") and (empty(bcf7_api_option("bcf7_live_secret_key")) or empty(bcf7_api_option("bcf7_live_collection_id")) or empty(bcf7_api_option("bcf7_live_xsignature_key"))) ) {
+			echo __(sprintf(
+				'<div class="notice notice-warning">
+							<p><strong>Billplz for Contact Form 7 -</strong>Billplz Live Credentials is not set. Enter your Secret Key, Collection ID and X-Signature Key in order to use Billplz service. <a href="' . get_admin_url() . 'admin.php?page=billplz-cf7&tab=api-settings">Set Credential</a></p>
+					</div>',
+			), BCF7_TEXT_DOMAIN);
 		}
 	}
 }
-
