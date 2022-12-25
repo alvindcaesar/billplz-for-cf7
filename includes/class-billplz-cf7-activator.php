623ef84df9f8d64fcd4095ed1b0f69d52e90ccee
Alvind <alvind.caesar@gmail.com>
2022-12-25 16:40:59 +0800

    Create and set default payment confirmation page

--- includes/class-billplz-cf7-activator.php
+++ includes/class-billplz-cf7-activator.php
@@ -21,34 +21,48 @@ class Billplz_CF7_Activator
 
     $page = get_page_by_title( $page_title );
 
+    $instance = new self();
+
     if (! empty( $page ) ) {
       if ( $page->post_content == $page_content ) {
         return;
       } else {
-        $page_id = \wp_insert_post(
-          array(
-            'post_title'     => __( 'BCF7 Payment Confirmation', 'billplz-for-cf7' ),
-            'post_name'      => 'bcf7-payment-confirmation',
-            'post_content'   => "<!-- wp:shortcode -->[bcf7_payment_confirmation]<!-- /wp:shortcode -->",
-            'post_status'    => 'publish',
-            'post_author'    => get_current_user_id(),
-            'post_type'      => 'page',
-            'comment_status' => 'closed'
-          )
-        );
+        $instance->save_id($instance->page_id());
       }
     } else {
-      $page_id = \wp_insert_post(
-        array(
-          'post_title'     => __( 'BCF7 Payment Confirmation', 'billplz-for-cf7' ),
-          'post_name'      => 'bcf7-payment-confirmation',
-          'post_content'   => "<!-- wp:shortcode -->[bcf7_payment_confirmation]<!-- /wp:shortcode -->",
-          'post_status'    => 'publish',
-          'post_author'    => get_current_user_id(),
-          'post_type'      => 'page',
-          'comment_status' => 'closed'
-        )
+      $instance->save_id($instance->page_id());
+    }
+  }
+
+  private function page_id()
+  {
+     return wp_insert_post(
+      array(
+        'post_title'     => __( 'BCF7 Payment Confirmation', 'billplz-for-cf7' ),
+        'post_name'      => 'bcf7-payment-confirmation',
+        'post_content'   => "<!-- wp:shortcode -->[bcf7_payment_confirmation]<!-- /wp:shortcode -->",
+        'post_status'    => 'publish',
+        'post_author'    => get_current_user_id(),
+        'post_type'      => 'page',
+        'comment_status' => 'closed'
+      )
+    );
+  }
+
+  private function save_id( $id )
+  {
+    $options = get_option("bcf7_general_settings");
+
+    if ( $options ) {
+      $options["bcf7_redirect_page"] = $id;
+      update_option( "bcf7_general_settings", $options );
+    } else {
+      $options = array(
+        "bcf7_mode" => "",
+        "bcf7_form_select" => "",
+        "bcf7_redirect_page" => $id
       );
+      add_option( "bcf7_general_settings", $options );
     }
   }
 }
