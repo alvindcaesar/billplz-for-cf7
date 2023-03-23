113fca1db295d472e5631d2add6c8e42c07cab94
Alvind <alvind.caesar@gmail.com>
2023-03-23 15:07:44 +0800

    Housekeeping

--- includes/views/api-settings-page.php
+++
@@ -1,8 +0,0 @@
-<form action="options.php" method="POST">
-  <?php
-    settings_fields( "bcf7_api" );
-    do_settings_sections( "bcf7_live_settings" );
-    do_settings_sections( "bcf7_sandbox_settings" );
-    submit_button();
-  ?>
-</form>
--- includes/views/general-settings-page.php
+++
@@ -1,7 +0,0 @@
-<form action="options.php" method="POST">
-  <?php
-    settings_fields( "bcf7_general" );
-    do_settings_sections( "bcf7_general_settings" );
-    submit_button();
-  ?>
-</form>
