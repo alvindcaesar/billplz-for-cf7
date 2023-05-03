5a07bef711583b1ecf125674fd680533ca56f225
Alvind <alvind.caesar@gmail.com>
2023-05-04 07:36:49 +0800

    Add plugin assets path constant

--- billplz-for-cf7.php
+++ billplz-for-cf7.php
@@ -22,6 +22,7 @@ if (file_exists( dirname(__FILE__) . '/vendor/autoload.php' ) ) {
 define("BCF7_PLUGIN_PATH", plugin_dir_path(__FILE__));
 define("BCF7_PLUGIN_URL", plugin_dir_url(__FILE__));
 define("BCF7_PLUGIN_FILE", plugin_basename(__FILE__));
+define("BCF7_ASSETS_URL", plugins_url('/billplz-for-cf7/assets/'));
 define("BCF7_TEXT_DOMAIN", "billplz-for-cf7");
 define("BCF7_PLUGIN_VERSION", "1.2");
 
@@ -29,4 +30,8 @@ if (class_exists('BillplzCF7\\Init')) {
   BillplzCF7\Init::register_services();
 }
 
-register_activation_hook( __FILE__, array( \BillplzCF7\Base\Activate::class, 'activate' ) );
+function bcf7_activation() {
+	( new \BillplzCF7\Base\Activate() )->activate();
+}
+
+register_activation_hook( __FILE__, 'bcf7_activation' );
