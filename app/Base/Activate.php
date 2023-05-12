2c326a7c3475eca1d75a0ea14ae537a0a71c37d0
Alvind Caesar <hello@alvindcaesar.com>
2023-05-13 05:43:03 +0800

    Refactor the `Activate` class

--- app/Base/Activate.php
+++ app/Base/Activate.php
@@ -13,7 +13,7 @@ use WP_Query;
  * Activate Class.
  */
 class Activate {
-	/**
+    /**
 	 * Method to activate the plugin.
 	 *
 	 * @return void
@@ -29,126 +29,165 @@ class Activate {
 	/**
 	 * Method to create a page.
 	 *
-	 * @return void
+	 * @param string $page_title The title of the page.
+	 *
+	 * @return int|null The ID of the created page or null if page already exists.
+	 */
+	private function create_page($page_title) {
+		$args = array(
+			'post_type'      => 'page',
+			'post_status'    => 'publish',
+			'posts_per_page' => 1,
+			's'              => $page_title,
+		);
+
+		$query = new WP_Query( $args );
+
+		if ( $query->have_posts() ) {
+			wp_reset_postdata();
+			return null;
+		} else {
+			$redirect_page = $this->insert_page($page_title);
+			$this->save_id($redirect_page);
+			return $redirect_page;
+		}
+	}
+
+    /**
+	 * Method to create the confirmation page.
+	 *
+	 * @return int|null The ID of the created page or null if page already exists.
 	 */
 	private function create_confirmation_page() {
-		$page_title   = esc_html__( 'BCF7 Payment Confirmation', BCF7_TEXT_DOMAIN );
-
-		$args = array(
-			'post_type' => 'page',
-			'post_status' => 'publish',
-			'posts_per_page' => 1,
-			's' => $page_title
-		);
-
-		$query = new WP_Query( $args );
-		
-		if ( $query->have_posts() ) {
-			wp_reset_postdata();
-			return;
-		} else {
-			$redirect_page = $this->page_id();
-			$this->save_id( $redirect_page );
-		}
+		$page_title = esc_html__( 'BCF7 Payment Confirmation', BCF7_TEXT_DOMAIN );
+		return $this->create_page($page_title);
 	}
 
-	/**
-	 * Method to create a BCF7 Example Form.
+    /**
+	 * Method to create the BCF7 Example Form.
 	 *
-	 * @return void
+	 * @return int|null The ID of the created form or null if form already exists.
 	 */
 	private function create_example_form() {
+		$form_title = 'BCF7 Example Payment Form';
+		$form_content = 'Payment Form Example';
 
 		$args = array(
-			'post_type' => 'wpcf7_contact_form',
+			'post_type'      => 'wpcf7_contact_form',
 			'posts_per_page' => 1,
-			'post_status' => 'publish',
-			's' => 'BCF7 Example Payment Form'
+			'post_status'    => 'publish',
+			's'              => $form_title,
 		);
-		
+
 		$query = new WP_Query( $args );
-		
+
 		if ( $query->have_posts() ) {
 			wp_reset_postdata();
-			return;
+			return null;
 		} else {
-			$post_data = array(
-				'post_title' => 'BCF7 Example Payment Form',
-				'post_content' => 'Payment Form Example',
-				'post_status' => 'publish',
-				'post_author' => get_current_user_id(),
-				'post_type' => 'wpcf7_contact_form',
-				'comment_status' => 'closed',
-			);
-		
-			$post_id = wp_insert_post($post_data);
-		
-			$form = '
-			<label> Name
-				[text* bcf7-name] </label>
-		
-			<label> Your email
-				[email* bcf7-email] </label>
-		
-			<label> Phone
-				[tel* bcf7-phone] </label>
-		
-			<label> Amount (RM)
-				[number* bcf7-amount] </label>
-		
-		
-			[submit "Submit"]';
-		
-			add_post_meta($post_id, '_additional_settings', 'skip_mail: on');
-			add_post_meta($post_id, '_form', $form);
+			$form_id = $this->insert_form($form_title, $form_content);
+			$this->add_form_meta($form_id);
+			return $form_id;
 		}
 	}
 
-	/**
+    /**
 	 * Method to insert a page and return its ID.
+	 *
+	 * @param string $page_title The title of the page.
 	 *
 	 * @return int The ID of the inserted page.
 	 */
-	private function page_id() {
+	private function insert_page($page_title) {
 		$post_id = wp_insert_post(
 			array(
-				'post_title'     => esc_html__( 'BCF7 Payment Confirmation', 'billplz-for-cf7' ),
-				'post_name'      => 'bcf7-payment-confirmation',
+				'post_title'     => $page_title,
+				'post_name'      => sanitize_title($page_title),
 				'post_content'   => '<!-- wp:shortcode -->[bcf7_payment_confirmation]<!-- /wp:shortcode -->',
 				'post_status'    => 'publish',
 				'post_author'    => get_current_user_id(),
 				'post_type'      => 'page',
 				'comment_status' => 'closed',
-			)
-		);
+			));
 
-		return $post_id;
-	}
+        return $post_id;
+    }
 
-	/**
-	 * Method to save the page ID in the options table.
-	 *
-	 * @param int $id The ID of the page.
-	 *
-	 * @return void
-	 */
-	private function save_id( $redirect_page_id ) {
-		$options = get_option( 'bcf7_general_settings' );
+    /**
+     * Method to save the page ID in the options table.
+     *
+     * @param int $redirect_page_id The ID of the page.
+     *
+     * @return void
+     */
+    private function save_id($redirect_page_id) {
+        $options = get_option('bcf7_general_settings');
 
-		if ( $options ) {
-			if ( ! empty( $options['bcf7_redirect_page'] ) && $options['bcf7_redirect_page'] === $redirect_page_id ) {
-				return;
-			} else {
-				$options['bcf7_redirect_page'] = $redirect_page_id;
-				update_option( 'bcf7_general_settings', $options );
-			}
-		} else {
-			$options = array(
-				'bcf7_mode'            => '1',
-				'bcf7_form_select'     => '',
-				'bcf7_redirect_page'   => $redirect_page_id,
-			);
-			add_option( 'bcf7_general_settings', $options );
-		}
-	}
+        if ($options) {
+            if (isset($options['bcf7_redirect_page']) && $options['bcf7_redirect_page'] === $redirect_page_id) {
+                return;
+            } else {
+                $options['bcf7_redirect_page'] = $redirect_page_id;
+                update_option('bcf7_general_settings', $options);
+            }
+        } else {
+            $options = array(
+                'bcf7_mode'            => '1',
+                'bcf7_form_select'     => '',
+                'bcf7_redirect_page'   => $redirect_page_id,
+            );
+            add_option('bcf7_general_settings', $options);
+        }
+    }
+
+    /**
+     * Method to insert a form and return its ID.
+     *
+     * @param string $form_title    The title of the form.
+     * @param string $form_content  The content of the form.
+     *
+     * @return int The ID of the inserted form.
+     */
+    private function insert_form($form_title, $form_content) {
+        $post_data = array(
+            'post_title'     => $form_title,
+            'post_content'   => $form_content,
+            'post_status'    => 'publish',
+            'post_author'    => get_current_user_id(),
+            'post_type'      => 'wpcf7_contact_form',
+            'comment_status' => 'closed',
+        );
+
+        $post_id = wp_insert_post($post_data);
+
+        return $post_id;
+    }
+
+    /**
+     * Method to add meta data for the form.
+     *
+     * @param int $form_id The ID of the form.
+     *
+     * @return void
+     */
+    private function add_form_meta($form_id) {
+        $form = '
+        <label> Name
+            [text* bcf7-name] </label>
+
+        <label> Your email
+            [email* bcf7-email] </label>
+
+        <label> Phone
+            [tel* bcf7-phone] </label>
+
+        <label> Amount (RM)
+            [number* bcf7-amount] </label>
+
+
+        [submit "Submit"]';
+
+        add_post_meta($form_id, '_additional_settings', 'skip_mail: on');
+        add_post_meta($form_id, '_form', $form);
+    }
 }
